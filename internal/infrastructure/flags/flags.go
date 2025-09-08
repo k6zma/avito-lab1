4fcae42949f2c1b9b9d18a0c53c1ae2e3def3c24
k6zma <87338187+k6zma@users.noreply.github.com>
2025-09-08 21:23:05 +0300

    feat(flags): add parsing of cli flags with subsequent validation
    What has been done:
    - all logic is moved to the infrastructure level;
    - add the `flags` package which stores the main logic of flags parsing;
    - added a flag to get information about the path to the storage file (name: data_path, default_value: students_data.json);
    - added custom validation for flag

---
+++ internal/infrastructure/flags/flags.go
@@ -0,0 +1,34 @@
+package flags
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/k6zma/avito-lab1/pkg/validators"
+)
+
+const (
+	dataFilePathFlagName         = "data_path"
+	dataFilePathFlagDefaultValue = "students_data.json"
+	dataFilePathFlagDesc         = "Path to file which will contains data about students"
+)
+
+var configPathFlag = flag.String(dataFilePathFlagName, dataFilePathFlagDefaultValue, dataFilePathFlagDesc)
+
+type StudyFlags struct {
+	ConfigPath string `validate:"required,filepath"`
+}
+
+func GetFlags() (*StudyFlags, error) {
+	flag.Parse()
+
+	result := &StudyFlags{
+		ConfigPath: *configPathFlag,
+	}
+
+	if err := validators.Validate.Struct(result); err != nil {
+		return nil, fmt.Errorf("error while validating flags in studify app: %w", err)
+	}
+
+	return result, nil
+}
